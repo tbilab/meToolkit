c5ff6362b22b69176497f028625c64286b7a5b85
Nick Strayer <nick.strayer@gmail.com>
2018-11-06 20:13:40 +0000

    Cleaned up description pallete generator to not depend on some random constants defined in a file for the default color palette.

--- R/makedescriptionpalette.R
+++ R/makedescriptionpalette.R
@@ -2,22 +2,45 @@
 #' Create Description Color Palette
 #'
 #' @param phecode_info Dataframe of phecodes included in the current view of the application
+#' @param color_palette Character vector of hex codes to be mapped to the descriptions in first come first filled order.
 #'
 #' @return A list containing a palette dataframe and a named array both containing mappings from description character string to hex code.
 #' @export
 #'
 #' @examples
-makeDescriptionPalette <- function(phecode_info){
+#' makeDescriptionPalette(myCurrentPhecodes)
+makeDescriptionPalette <- function(
+  phecode_info,
+  color_palette = c(
+    "#895de6",
+    "#6ecc3b",
+    "#e34bca",
+    "#9ad843",
+    "#ff3d97",
+    "#01d8a6",
+    "#ff4254",
+    "#64c5ff",
+    "#957100",
+    "#0074d3",
+    "#aad366",
+    "#ff8dcc",
+    "#4e5b00",
+    "#ffb38f",
+    "#325c2c",
+    "#faba5f",
+    "#8b373b",
+    "#7e431e" )
+){
 
-  unique_descriptions <- phecode_info$category %>% unique()
+  unique_descriptions <- unique(phecode_info$category)
 
-
-  palette <- data_frame(
+  palette <- tibble::tibble(
     description = unique_descriptions,
-    color = CATEGORY_COLORS %>% head(length(unique_descriptions))
+    color = color_palette %>% head(length(unique_descriptions))
   )
 
-  palette_array <- palette$color # turn dataframe into a named array for ggplot scale
+  # turn dataframe into a named array for ggplot scale
+  palette_array <- palette$color
   names(palette_array) <- palette$description
 
   list(
