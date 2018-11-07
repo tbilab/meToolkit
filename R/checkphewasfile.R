e5f2a4dbb1327ad653d8509cfaf190fc78584e43
Nick Strayer <nick.strayer@gmail.com>
2018-11-07 17:11:49 +0000

    Added phewas input checking function.

---
+++ R/checkphewasfile.R
@@ -0,0 +1,33 @@
+
+#' Check uploaded phewas file for correctness
+#'
+#' @param phewas Dataframe from uploaded phewas csv
+#'
+#' @return Supplied dataframe with a tooltip column added if it wasn't originally present.
+#' @export
+#'
+#' @examples
+#' checkPhewasFile('uploadedPhewasFile')
+checkPhewasFile <- function(phewas){
+
+  columns <- colnames(phewas)
+
+  has_code <- 'code' %in% columns
+  if(!has_code) stop("Missing Code column.", call. = FALSE)
+
+  has_category <- 'category' %in% columns
+  if(!has_category) stop("Missing Category column", call. = FALSE)
+
+  has_pval <- 'p_val' %in% columns
+  if(!has_pval) stop("Missing P Value column (p_val)", call. = FALSE)
+
+  # Do we have a supplied tooltip already or should we make one from the left over
+  # columns?
+  has_tooltip <- 'tooltip' %in% columns
+
+  if(!has_tooltip){
+    phewas <- phewas %>% meToolkit::makeTooltips()
+  }
+
+  phewas
+}
