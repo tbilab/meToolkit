485319cd8ef33ed3c13b2dd0c48328d3e9c6d30b
Nick Strayer <nick.strayer@gmail.com>
2019-01-04 23:40:37 +0000

    Updated network plot test app to actually run but havent wired up the network module yet as it does not exist yet

--- demo_apps/comorbidityNetwork2d_app.R
+++ demo_apps/comorbidityNetwork2d_app.R
@@ -1,16 +1,19 @@
+library(shiny)
+library(shinydashboard)
 library(meToolkit)
+library(here)
 
 
 # testing main app module in own shiny app.
+n_codes <- 9
+n_cases <- 200
 
-cached_data <- read_rds(here('data/network_data.rds'))
-
-subset_data <- cached_data$subset_data
-results_data <- (cached_data$results_data) %>%
+individual_data <- meToolkit::simIndividualData(n_codes = n_codes, n_cases = n_cases, prob_snp = 0.1)
+phewas_results <- meToolkit::simPhewasResults(n_codes = n_codes, n_categories = 3) %>%
   meToolkit::buildColorPalette(category)
-inverted_codes <- cached_data$inverted_codes
-snp_filter <- cached_data$snp_filter
 
+inverted_codes <- c()
+snp_filter <- FALSE
 
 ui <- shinyUI(
   dashboardPage(
@@ -26,27 +29,28 @@ ui <- shinyUI(
       collapsed = TRUE
     ),
     dashboardBody(
-      includeCSS(here("www/test_app.css")),
-      network_plots_UI('network_plots', I)
+      # includeCSS(here("www/test_app.css")),
+      # network_plots_UI('network_plots', I),
+      h1('Hi There')
     ),
     skin = 'black'
   )
 )
 
 server <- function(input, output, session) {
-  networks <- callModule(
-    network_plots, 'network_plots',
-    subset_data = subset_data,
-    results_data = results_data,
-    inverted_codes = inverted_codes,
-    parent_ns = session$ns,
-    snp_filter = TRUE # THIS NEEDS TO BE WIRED UP PROPERLY
-  )
-
-  observeEvent(input$message, {
-    print('message from network')
-    print(input$message)
-  })
+  # networks <- callModule(
+  #   network_plots, 'network_plots',
+  #   subset_data = subset_data,
+  #   results_data = results_data,
+  #   inverted_codes = inverted_codes,
+  #   parent_ns = session$ns,
+  #   snp_filter = TRUE # THIS NEEDS TO BE WIRED UP PROPERLY
+  # )
+  #
+  # observeEvent(input$message, {
+  #   print('message from network')
+  #   print(input$message)
+  # })
 }
 
 shinyApp(ui, server)
