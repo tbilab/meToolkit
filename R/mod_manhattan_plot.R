a483b4b4b8a9827f5eda3fabe02166b6e8f50419
Nick Strayer <nick.strayer@gmail.com>
2019-03-18 22:18:10 +0000

    Filled out roxygen documentation and added the required inputs to the server function.

--- R/mod_manhattan_plot.R
+++ R/mod_manhattan_plot.R
@@ -1,7 +1,7 @@
 #' UI function of manhattan plot module. This module produces a typical manhattan plot with interactive code selection
 #'
 #' @param id String with unique id of module in app
-#' @return HTML component of shiny module
+#' @return UI component of interactive manhattan plot
 #' @export
 #'
 #' @examples
@@ -9,17 +9,24 @@
 manhattan_plot_UI <- function(id) {
   ns <- NS(id)
   tagList(
-
+    div(
+      plotly::plotlyOutput(ns("manhattanPlot"), height = '100%')
+    )
   )
 }
 #' Server function of manhattan plot module. Returns a reactive variable that dispenses interaction events in the common type, payload format of \code{meToolkit} modules.
 #'
 #' @param input,output,session Auto-filled by callModule | ignore
-#' @return Shiny module
+#' @param results_data Dataframe containing the results of the phewas study. Needs columns \code{p_val}, \code{id}, \code{category}, \code{tooltip}.
+#' @param selected_codes A reactive variable containing array of code \code{id}s that are currently selected in the app.
+#' @return Server component of interactive manhattan plot
 #' @export
 #'
 #' @examples
-#' callModule(manhattan_plot, 'my_mod')
-manhattan_plot <- function(input, output, session) {
+#' callModule(manhattan_plot, selected_codes, app_state$currently_selected, 'my_mod')
+manhattan_plot <- function(
+  input, output, session,
+  results_data,
+  selected_codes ) {
 
 }
