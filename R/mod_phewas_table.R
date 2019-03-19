2a6f48e20a08e8b72f86f0a523e489dfd18e556d
Nick Strayer <nick.strayer@gmail.com>
2019-03-19 20:37:55 +0000

    Phewas table module is in and working nice and smoothly with the new module format.

--- R/mod_phewas_table.R
+++ R/mod_phewas_table.R
@@ -8,8 +8,13 @@
 #' phewas_table_UI('my_mod')
 phewas_table_UI <- function(id) {
   ns <- NS(id)
-  tagList(
-
+  shiny::tagList(
+    DT::dataTableOutput(ns('selected_codes_list')),
+    shiny::div(
+      id = 'table_selection',
+      shiny::span('Click on rows to unselect codes.'),
+      shiny::actionButton(ns("filter_button"), "Update")
+    )
   )
 }
 
@@ -24,6 +29,48 @@ phewas_table_UI <- function(id) {
 #'
 #' @examples
 #' callModule(phewas_table, 'my_mod')
-phewas_table <- function(input, output, session) {
+phewas_table <- function(
+  input, output, session,
+  results_data,
+  selected_codes,
+  action_object ) {
 
+  # Reactive object containing the dataframe of just the currently shown codes
+  displayed_codes <- reactive({
+    results_data %>%
+      filter(code %in% selected_codes()) %>%
+      arrange(p_val) %>%
+      select(-tooltip, -color)
+  })
+
+  # Datatable output
+  output$selected_codes_list <- DT::renderDataTable({
+    displayed_codes() %>%
+      rename(`P-Value` = p_val) %>%
+      DT::datatable(
+        options = list(
+          scrollY = 200,
+          scroller = TRUE,
+          dom = 't',
+          order = list(list(3, 'asc')),
+          pageLength = nrow(.)
+        ),
+        selection = list('multiple', selected = 1:nrow(.))
+      )
+  })
+
+  # Observe the pressing of the filter button and send message back to the action element.
+  observeEvent(input$filter_button, {
+
+    codes_to_keep <- displayed_codes()$code[input$selected_codes_list_rows_selected]
+    some_codes_deleted <- length(codes_to_keep) != nrow(displayed_codes())
+
+    # Only submit the action if it's needed
+    if(some_codes_deleted){
+      action_object(list(
+        type = 'selection',
+        payload = codes_to_keep
+      ))
+    }
+  })
 }
