85480ba98d9a2f742b6bcb796a4b6c7aa03b8efa
Nick Strayer <nick.strayer@gmail.com>
2019-03-28 17:28:31 +0000

    Network is decently stable at this point and can be easily switched between bipartite and free layouts.

--- inst/d3/pattern_network/helpers.js
+++ inst/d3/pattern_network/helpers.js
@@ -44,10 +44,112 @@ function build_nodes_links(patterns, codes, C){
     }))
   ];
 
-  if(C.layout === 'bipartite'){
+  if(C.bipartite){
     nodes.forEach(d => {
-      d.fx = d.type === 'pattern' ? (2*C.w)/3:  C.w/3;
+      d.fx = C.w * (d.type === 'pattern' ? 0.8:  0.2);
     });
-  return {nodes, links}
+  }
+
+  return {nodes, links};
 }
+
+function setup_scales(nodes, C){
+  // How many total pattern nodes do we have?
+  const patterns = nodes.filter(d => d.type === 'pattern');
+  const n_patterns = patterns.length;
+  // What's the average radius of a given node going to need to be
+  // to fit on the vertical space we have?
+  const average_node_r = (C.h/n_patterns) - (C.radius_exageration/2);
+
+  // Scale to size the nodes in network
+  const size = d3.scaleSqrt()
+    .domain([0, d3.max(patterns, d => d.total_size)])
+    .range([0, average_node_r]);
+
+  const color = d3.scaleLinear()
+    .domain(d3.extent(nodes, d => d.snp_ratio))
+    .range([C.no_snp_color, C.snp_color]);
+
+  return {size, color};
+}
+
+function setup_network(nodes, links, g, scales, C){
+
+  // Setup the chart components
+  const link_lines = g.selectAppend("g.links")
+    .at({ stroke: "#999", strokeOpacity: 0.6 })
+      .selectAll("line")
+      .data(links)
+      .enter()
+      .append('line')
+      .at({strokeWidth: 1});
+
+  const node_gs = g.selectAppend("g.nodes")
+    .selectAll('circle')
+    .data(nodes)
+    .enter().append('g.node');
+
+  const node_circles = node_gs.append('circle')
+    .at({
+      stroke: 'darkgrey',
+      strokeWidth: 1,
+      r: d => d.type === 'pattern' ? scales.size(d.total_size): C.code_radius,
+      fill: d => d.type === 'pattern' ? scales.color(d.snp_ratio): C.code_color
+    });
+
+  const code_text = node_gs.filter(d => d.type === 'code')
+    .append('text')
+    .at({
+      textAnchor: 'middle',
+      alignmentBaseline: 'middle',
+      fill: 'white',
+    })
+    .st({
+      fontSize: '0.9em',
+    })
+    .text(d => d.name);
+
+  node_gs
+    .on('mouseover', d => {
+      console.log(d.name);
+    });
+
+  function update(){
+    node_circles
+      .at({
+        cx: d => {
+          if(C.pin_in_window){
+            const radius = scales.size(d.total_size);
+            d.x = Math.max(radius, Math.min(C.w - radius, d.x));
+          }
+          return d.x;
+        },
+        cy: d => {
+          if(C.pin_in_window){
+            const radius = scales.size(d.total_size);
+            d.y = Math.max(radius, Math.min(C.h - radius, d.y));
+          }
+          return d.y;
+        }
+      });
+
+  code_text
+    .at({
+      x: d => d.x,
+      y: d => d.y
+    });
+
+    link_lines
+      .at({
+        x1: d => d.source.x,
+        x2: d => d.target.x,
+        y1: d => d.source.y,
+        y2: d => d.target.y,
+      });
+  }
+
+  return {
+    update,
+    set_call: f => node_circles.call(f),
+  };
 }
--- inst/d3/pattern_network/main.js
+++ inst/d3/pattern_network/main.js
@@ -1,7 +1,5 @@
 // !preview r2d3 data = read_rds(here('module_tests/data/upset_r2d3_data.rds')), dependencies = c("d3-jetpack",here('inst/d3/pattern_network/helpers.js'))
 
-console.log('im here!');
-
 // Constants
 const margin = {right: 25, left: 25, top: 20, bottom: 70}; // margins on side of chart
 
@@ -16,12 +14,13 @@ const C = Object.assign({
   margin: margin,
   edge_color: '#aaa',
   msg_loc: 'shiny_server',
-  layout: 'bipartite',
-  radius_exageration: 10,
+  bipartite: false,
+  radius_exageration: 15,
   code_radius: 25,
   snp_color: '#fc8d59',
   no_snp_color: '#99d594',
   code_color: '#beaed4',
+  pin_in_window: true,
 }, options);
 
 const viz_g = svg.selectAppend('g.viz')
@@ -33,108 +32,84 @@ const codes = data.options.marginalData;
 
 const {nodes, links} = build_nodes_links(patterns, codes, C);
 
+const scales = setup_scales(nodes, C);
+const network = setup_network(nodes, links, viz_g, scales, C);
 
-// Scale to size the nodes in network
-const node_size_scale = d3.scaleSqrt()
-  .domain([0, d3.max(patterns, d => d.count)])
-  .range([0, 50]);
+const collision_detection = (exageration) => d3.forceCollide()
+  .radius(d =>
+    (d.type === 'pattern' ?
+      scales.size(d.total_size):
+      C.code_radius) + exageration
+  )
+  .strength(0.7);
 
-const pattern_color_scale = d3.scaleLinear()
-  .domain(d3.extent(nodes, d => d.snp_ratio))
-  .range([C.no_snp_color, C.snp_color]);
+const link_force = d3.forceLink(links)
+  .id(d => d.name)
+  .strength(0.6)
+  .iterations(25);
+
 
 
 // Setup the simulation
 const simulation = d3.forceSimulation(nodes)
-  .force("link",
-    d3.forceLink(links)
-      .id(d => d.name)
-  )
- .force('collision',
-    d3.forceCollide()
-      .radius(d =>
-        (d.type === 'pattern' ?
-          node_size_scale(d.total_size):
-          C.code_radius) + C.radius_exageration
-      )
-      .strength(0.8)
-  )
-  .force("charge", d3.forceManyBody())
-  //.force("x", d3.forceX(C.w / 2))
-  .force("y", d3.forceY(C.h / 2));
+  .alphaDecay(0.02)
+  .force("link", link_force)
+  .force('collision',collision_detection(C.radius_exageration))
+  .force("charge",d3.forceManyBody())
+  .force("y",d3.forceY(C.h/2).strength(0.7));
 
-// Setup the chart components
-const link_lines = viz_g.selectAppend("g.links")
-  .at({ stroke: "#999", strokeOpacity: 0.6 })
-    .selectAll("line")
-    .data(links)
-    .enter()
-    .append('line')
-    .at({strokeWidth: 3});
+if(!C.bipartite){
+  simulation.force('x', d3.forceX(C.w/2).strength(0.7));
+}
 
-const node_circles = viz_g.selectAppend("g.nodes")
-  .at()
-  .selectAll('circle')
-  .data(nodes)
-  .enter().append('g.node');
+let i = 1;
+const num_ticks = 300;
+simulation.on('tick', () => {
+  i++;
+  if(i === Math.ceil(num_ticks*0.8)){
+    simulation
+      .force('collision',
+            collision_detection(C.radius_exageration*0.35)
+      );
+  }
 
-node_circles.append('circle')
-  .at({
-    stroke: '#fff',
-    strokeWidth: 1.5,
-    r: d => d.type === 'pattern' ? node_size_scale(d.total_size): C.code_radius,
-    fill: d => d.type === 'pattern' ? pattern_color_scale(d.snp_ratio): C.code_color
-  })
-  .call(drag(simulation));
-
-node_circles.filter(d => d.type === 'code')
-  .append('text')
-  .at({
-    //x: - C.code_radius,
-    textAnchor: 'middle',
-    alignmentBaseline: 'middle',
-    fill: 'white',
-  })
-  .st({
-    fontSize: '0.9em',
-  })
-  .text(d => d.name)
-
-node_circles
-  .on('mouseover', d => {
-    console.log(d.name);
-  });
-
-simulation.on("tick", () => {
-  link_lines
-    .at({
-      x1: d => d.source.x,
-      x2: d => d.target.x,
-      y1: d => d.source.y,
-      y2: d => d.target.y,
-    });
-
-  node_circles
-    .translate(d => [d.x, d.y]);
+  network.update();
 });
 
+
+network.set_call(drag(simulation));
 
 function drag(simulation){
 
   function dragstarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    //d.fx = d.x;
+    if (!d3.event.active) {
+      simulation
+        .alphaTarget(0.1)
+        .restart();
+    }
+    if(!C.bipartite){
+      d.fx = d.x;
+    }
     d.fy = d.y;
   }
 
   function dragged(d) {
-   // d.fx = d3.event.x;
+    if(!C.bipartite){
+      d.fx = d3.event.x;
+    }
     d.fy = d3.event.y;
   }
 
   function dragended(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    //d.fx = null;
+    if (!d3.event.active) {
+      simulation
+        .alphaTarget(0);
+    }
+
+    if(!C.bipartite){
+      d.fx = null;
+    }
+
     d.fy = null;
   }
 
