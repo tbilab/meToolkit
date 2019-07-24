2df7c5e0ca1632b9b34c62e90e41e3c3f8ca373e
Nick Strayer <nick.strayer@gmail.com>
2019-07-24 16:59:28 +0000

    Manhattan plot now has points and an axis and semi-clear selection of points

--- inst/d3/manhattan_plot/manhattan_plot.js
+++ inst/d3/manhattan_plot/manhattan_plot.js
@@ -1,42 +1,54 @@
-// !preview r2d3 data=phewas_results, options=list(selected=first_selected), container = 'div', dependencies = c(here::here('inst/d3/manhattan_plot/rx.js'))
+// !preview r2d3 data=phewas_results, options=list(selected=first_selected), container = 'div', dependencies = c(here::here('inst/d3/manhattan_plot/rx.js'), 'd3-jetpack')
 // ===============================================================
 // Initialization
 // This code is run a single time
 // ===============================================================
-const margin = {left: 10, right: 10, top: 10, bottom: 10};
+d3.selection.prototype.last = function() {
+  return d3.select(
+      this.nodes()[this.size() - 1]
+  );
+};
+
+const margin = {left: 65, right: 10, top: 10, bottom: 10};
 const manhattan_prop = 0.6;
 
-const main_viz = div.append('svg')
-  .attr('id', 'main_viz')
-  .attr('height', height*0.6)
-  .attr('width', width - margin.left - margin.right)
-  .style('background', 'steelblue');
 
-const or_hist = div.append('svg')
+const main_svg = div.append('svg')
+  .attr('id', 'main_viz');
+
+const main_viz = main_svg
+  .append('g')
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
+const or_svg = div.append('svg')
   .attr('id', 'or_hist')
-  .attr('height', height*0.2)
-  .attr('width', width - margin.left - margin.right)
   .style('background', 'forestgreen');
 
+const or_hist = or_svg
+  .append('g')
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
+const manhattan_scales = {
+  x: d3.scaleLinear(),
+  y: d3.scaleLinear(),
+};
 
 const {scan, shareReplay} = rxjs.operators;
 const app_state = setup_state();
-
 
 // ===============================================================
 // Rendering
 // This code runs whenever data changes
 // ===============================================================
 r2d3.onRender(function(data, svg, width, height, options) {
-  const main_sizes = {
-    h: (height - margin.top - margin.bottom) * manhattan_prop,
-    w: width - margin.left - margin.right,
-  };
+  // Make sure viz is sized correctly.
+  size_viz(width, height);
 
   let log_pval_max = 0,
       log_or_min = 0,
       log_or_max = 0;
 
+  // Add a log_pval and log_or field to all points and keep track of extents
   data.forEach(d => {
     d.log_pval = -Math.log10(d.p_val);
     if(d.log_pval > log_pval_max) log_pval_max = d.log_pval;
@@ -44,10 +56,62 @@ r2d3.onRender(function(data, svg, width, height, options) {
     d.log_or = Math.log(d.OR);
     if(d.log_or < log_or_min) log_or_min = d.log_or;
     if(d.log_or > log_or_max) log_or_max = d.log_or;
+
+    d.unselected_color = d3.interpolateLab(d.color, "white")(0.7);
   });
 
-
+  // Update the domains for the manhattan plot
+  manhattan_scales.x.domain([0, data.length]);
+  manhattan_scales.y.domain([0,log_pval_max]).nice();
+  draw_manhattan(options.selected);
 });
+
+
+function draw_manhattan(selected_codes){
+
+  const any_selected = selected_codes.length !== 0;
+
+  const manhattan_points = main_viz.selectAll('circle')
+    .data(data, d => d.code);
+
+  manhattan_points.enter()
+    .append('circle')
+    .merge(manhattan_points)
+    .attr('cx', (d,i) => manhattan_scales.x(i))
+    .attr('cy', (d,i) => manhattan_scales.y(d.log_pval))
+    .attr('r', (d,i) => 2)
+    .attr('fill', (d,i) => d[ !any_selected || selected_codes.includes(d.code) ? 'color': 'unselected_color']);
+
+  const y_axis = main_viz.selectAppend("g#y-axis")
+    .call(function(g){
+      g.attr("transform", `translate(${-5},0)`)
+      .call(d3.axisLeft(manhattan_scales.y).tickSizeOuter(0));
+    });
+
+  y_axis.selectAll('text')
+    .last()
+    .text('-Log10 P');
+
+}
+
+function size_viz(width, height){
+  const manhattan_height = height*manhattan_prop;
+  const or_height = height*(1 - manhattan_prop);
+
+  // Adjust the sizes of the svgs
+  main_svg
+    .attr('height', manhattan_height)
+    .attr('width', width);
+
+  or_svg
+    .attr('height', or_height)
+    .attr('width', width);
+
+  // Calculate the sizes needed and return scales for use
+  // Update the scale ranges
+  manhattan_scales.x.range([0, width - margin.left - margin.right]);
+  manhattan_scales.y.range([manhattan_height - margin.top - margin.bottom, 0]);
+}
 
 // ===============================================================
 // Resizing
@@ -55,10 +119,10 @@ r2d3.onRender(function(data, svg, width, height, options) {
 // ===============================================================
 
 r2d3.onResize(function(width, height) {
-  console.log('The plot was just resized!')
-
+  console.log('The plot was just resized!');
+  size_viz(width, height);
+  draw_manhattan(options.selected);
 });
-
 
 
 
@@ -118,4 +182,9 @@ function process_action(state, {type, payload}) {
       console.log('unknown input');
   }
   return new_state;
+}
+
+function desaturate(color, k = 1) {
+  const {l, c, h} = d3.lch(color);
+  return d3.lch(l, c + 18 * k, h);
 }
