01903c91ff976e976dac2286f85782b8a06c8987
Nick Strayer <nick.strayer@gmail.com>
2019-08-30 18:38:33 +0000

    Namespace package functions

--- R/mod_data_loader.R
+++ R/mod_data_loader.R
@@ -48,12 +48,11 @@ data_loader <- function(
   preloaded_path = NULL
 ) {
 
-
   #----------------------------------------------------------------
   # Reactive Values based upon user input
   #----------------------------------------------------------------
   print('running loading module!')
-  app_data <- reactiveValues(
+  app_data <- shiny::reactiveValues(
     phenome_raw = NULL,
     genome_raw = NULL,
     phewas_raw = NULL,
@@ -70,9 +69,9 @@ data_loader <- function(
     preloaded_snps <- list.files(preloaded_path, pattern = 'rs')
     if(length(preloaded_snps) > 0){
       # Only show selector if there actually is data
-      output$preloaded_snps <- renderUI({
+      output$preloaded_snps <- shiny::renderUI({
         shiny::tagList(
-          selectInput(
+          shiny::selectInput(
             session$ns("dataset_selection"),
             "Select a pre-loaded dataset:",
             preloaded_snps
@@ -85,33 +84,33 @@ data_loader <- function(
   }
 
 
-  observeEvent(input$genome, {
+  shiny::observeEvent(input$genome, {
 
     tryCatch({
-      good_genome_file <- read_csv(input$genome$datapath) %>%
-        checkGenomeFile()
+      good_genome_file <- readr::read_csv(input$genome$datapath) %>%
+        meToolkit::checkGenomeFile()
 
       app_data$snp_name <- good_genome_file$snp_name
       app_data$genome_raw <- good_genome_file$data
     },
     error = function(message){
       print(message)
-      pretty_popup(
+      meToolkit::pretty_popup(
         session,
         "There's something wrong with the format of your genome data",
-        " Make sure the file has two columns. One with the title IID with unique id and one with the title of your snp containing copies of the minor allele."
+        "Make sure the file has two columns. One with the title IID with unique id and one with the title of your snp containing copies of the minor allele."
       )
     })
   })
 
-  observeEvent(input$phewas, {
+  shiny::observeEvent(input$phewas, {
 
     tryCatch({
-      app_data$phewas_raw <- read_csv(input$phewas$datapath) %>% checkPhewasFile()
+      app_data$phewas_raw <- meToolkit::checkPhewasFile(readr::read_csv(input$phewas$datapath))
     },
     error = function(message){
       print(message)
-      pretty_popup(
+      meToolkit::pretty_popup(
         session,
         "There's something wrong with the format of your results data.",
         "Make sure the file has the right columns as listed."
@@ -119,13 +118,13 @@ data_loader <- function(
     })
   })
 
-  observeEvent(input$phenome, {
+  shiny::observeEvent(input$phenome, {
     tryCatch({
-      app_data$phenome_raw <- read_csv(input$phenome$datapath) %>% checkPhenomeFile()
+      app_data$phenome_raw <- meToolkit::checkPhenomeFile(readr::read_csv(input$phenome$datapath))
     },
     error = function(message){
       print(message)
-      pretty_popup(
+      meToolkit::pretty_popup(
         session,
         "There's something wrong with the format of your phenome data.",
         "Make sure the file has the right columns as listed."
@@ -137,20 +136,20 @@ data_loader <- function(
   # Data Loading Logic
   #----------------------------------------------------------------
   # Watches for all files to be loaded and then triggers.
-  observe({
-    req(app_data$phewas_raw, app_data$genome_raw, app_data$phenome_raw)
+  shiny::observe({
+    shiny::req(app_data$phewas_raw, app_data$genome_raw, app_data$phenome_raw)
 
-    withProgress(message = 'Loading data', value = 0, {
+    shiny::withProgress(message = 'Loading data', value = 0, {
       # read files into R's memory
-      incProgress(1/3, detail = "Reading in uploaded files")
+      shiny::incProgress(1/3, detail = "Reading in uploaded files")
 
       phenome <- app_data$phenome_raw
       genome  <- app_data$genome_raw
       phewas  <- app_data$phewas_raw
 
       # first spread the phenome data to a wide format
-      incProgress(2/3, detail = "Processing phenome data")
-      individual_data <- mergePhenomeGenome(phenome, genome)
+      shiny::incProgress(2/3, detail = "Processing phenome data")
+      individual_data <- meToolkit::mergePhenomeGenome(phenome, genome)
 
       # These are codes that are not shared between the phewas and phenome data. We will remove them
       # from either.
@@ -162,25 +161,24 @@ data_loader <- function(
 
       # remove bad codes from phewas and individual data if needed
       if(length(bad_codes) > 0){
-        app_data$phewas_data <- filter(app_data$phewas_data, !(code %in% bad_codes))
+        app_data$phewas_data <- dplyr::filter(app_data$phewas_data, !(code %in% bad_codes))
         app_data$individual_data <- app_data$individual_data[,-app_data$individual_data(phenome_cols %in% bad_codes)]
       }
 
       # Sending to app
-      incProgress(3/3, detail = "Sending to application!")
+      shiny::incProgress(3/3, detail = "Sending to application!")
 
       app_data$data_loaded <- TRUE
     }) # end progress messages
   })
 
 
-  observeEvent(input$preLoadedData,{
-    base_dir <- glue('data/preloaded/{input$dataset_selection}')
+  shiny::observeEvent(input$preLoadedData,{
+    base_dir <- glue::glue('{preloaded_path}/{input$dataset_selection}')
 
-    app_data$phewas_raw <- glue('{base_dir}/phewas_results.csv') %>% read_csv()
-    app_data$phenome_raw <- read_csv('data/preloaded/id_to_code.csv')
-    genome_file <- glue('{base_dir}/id_to_snp.csv') %>%
-      read_csv() %>% {
+    app_data$phewas_raw <- readr::read_csv(glue::glue('{base_dir}/phewas_results.csv'))
+    app_data$phenome_raw <- readr::read_csv('{preloaded_path}/id_to_code.csv')
+    genome_file <- readr::read_csv( glue::glue('{base_dir}/id_to_snp.csv') ) %>% {
         this <- .
 
         # Hacky fix for having id as both IID and grid in different datasets.
@@ -190,13 +188,13 @@ data_loader <- function(
 
         this
       } %>%
-      checkGenomeFile()
+      meToolkit::checkGenomeFile()
     app_data$snp_name <- genome_file$snp_name
     app_data$genome_raw <- genome_file$data
   })
 
   return(
-    reactive({
+    shiny::reactive({
       if(app_data$data_loaded){
         list(
           individual_data = app_data$individual_data,
