c0467fd4a2ca27bddbeb2251d21936e8e8a37de3
Nick Strayer <nick.strayer@gmail.com>
2019-09-03 18:53:45 +0000

    Added a function that takes data as an input and returns a full shiny app so it's easy to build and deploy an app on shinyapps or connect.

---
+++ R/build_me_app.R
@@ -0,0 +1,36 @@
+#' Build ME app from data
+#' Takes as input the three basic components of an ME app and returns a list containing the ui and server parts of an entire app. This makes it easy to deploy a custom app with a few lines of code.
+#' @param snp_name Character string containing the RSID of the snp you're viewing. Used to find annotation information.
+#' @param results_data Dataframe containing the results of the phewas study. Needs columns \code{p_val}, \code{id}, \code{category}(along with accompanying \code{color}), \code{tooltip}.
+#' @param individual_data Dataframe containing columns on \code{IID}, \code{snp}(# copies of allele), and columns for each code included.
+#' @param max_allowed_codes How many codes can the app show at any given time. Defaults to 40. (Too many and app may get slow.)
+#'
+#' @return A list containing elements: `ui` for the shiny app ui function, and `server` for the server component of app.
+#' @export
+#'
+#' @examples
+#' my_ME_app <- build_me_app(
+#'   snp_name        = 'rs13283456',
+#'   results_data    = read_rds('data/simulated_phewas_results.rds') ,
+#'   individual_data = read_rds('data/simulated_ind_data.rds')
+#' )
+#' shinyApp(my_ME_app$ui, my_ME_app$server)
+build_me_app <- function(snp_name, results_data, individual_data, max_allowed_codes = 45){
+
+  list(
+    ui = shiny::htmlTemplate(
+      system.file("html_templates/empty_page.html", package = "meToolkit"),
+      app_content = meToolkit::main_dashboard_UI("main_app")
+    ),
+
+    server = function(input, output, session) {
+      shiny::callModule(
+        meToolkit::main_dashboard, 'main_app',
+        snp_name           = snp_name,
+        results_data       = results_data ,
+        individual_data    = individual_data,
+        max_allowed_codes  = max_allowed_codes
+      )
+    }
+  )
+}
--- inst/demo_app/app.R
+++ inst/demo_app/app.R
@@ -4,34 +4,10 @@ library(meToolkit)
 library(readr)
 
 
-usage_instructions <- div(
-  h2('How To Use'),
-  h3("Manhattan Plot"),
-  p("Use the Manhattan plot to select your codes of interest by dragging a box on main plot or searching/selecting with the table."),
-  p("Once you have your desired codes selected press 'Update Network' button at top of pane to update the network data with individuals possessing the selected codes."),
-  h3("Upset Plot"),
-  p("The upset plot allows you to see basic statistics about comorbidity patterns in the selected subset of codes, such as number of patients with a pattern and the risk of that pattern occuring in individuals with at least one copy of the minor allele."),
-  p("Clicking on a given pattern in the upset plot will highlight the patients with that pattern in the below network plot."),
-  h3("Network Plot"),
-  p("The network plot provides a direct look at the individual-level data. You can click on codes to select them for isolation or deletion from the current selection.")
+my_ME_app <- build_me_app(
+  snp_name        = 'rs13283456',
+  results_data    = read_rds('data/simulated_phewas_results.rds') ,
+  individual_data = read_rds('data/simulated_ind_data.rds')
 )
 
-ui <- htmlTemplate(
-  system.file("html_templates/empty_page.html", package = "meToolkit"),
-  app_content = main_dashboard_UI("main_app")
-)
-
-
-server <- function(input, output, session) {
-  callModule(
-    main_dashboard, 'main_app',
-    snp_name           = 'rs13283456',
-    results_data       = read_rds('data/simulated_phewas_results.rds') ,
-    individual_data    = read_rds('data/simulated_ind_data.rds'),
-    max_allowed_codes  = 45,
-    usage_instructions = usage_instructions
-  )
-
-}
-
-shinyApp(ui, server)
+shinyApp(my_ME_app$ui, my_ME_app$server)
