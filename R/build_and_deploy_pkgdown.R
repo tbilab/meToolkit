54be9aadc721d232035496a96f632c988bcff80d
Nick Strayer <nick.strayer@gmail.com>
2019-09-16 22:35:21 +0000

    Rebuilt pkgdown after updating license.

---
+++ R/build_and_deploy_pkgdown.R
@@ -0,0 +1,15 @@
+# Build site
+pkgdown::build_site()
+
+# Deploy package site to RStudio connect
+site_loc <- here::here('docs/')
+rsconnect::deployApp(
+  site_loc,                          # the directory containing the content
+  appFiles = list.files(site_loc, recursive = TRUE), # the list of files to include as dependencies (all of them)
+  appPrimaryDoc = "index.html",                 # the primary file
+  appName = "meToolkit",                      # name of the endpoint (unique to your account on Connect)
+  appTitle = "meToolkit Package",                  # display name for the content
+  account = 'nstrayer',                # your Connect username
+  server = 'prod.tbilab.org'                     # the Connect server, see rsconnect::accounts()
+)
+
