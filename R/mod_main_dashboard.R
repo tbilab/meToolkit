2c4dbf8c5757e06982b37f2dffc8ef83f2fa1d48
Nick Strayer <nick.strayer@gmail.com>
2019-09-16 19:14:36 +0000

    Made names in instructions consistant with section headers.

--- R/mod_main_dashboard.R
+++ R/mod_main_dashboard.R
@@ -62,13 +62,13 @@ main_dashboard <- function(
   if(usage_instructions == 'default'){
     app_instructions <- div(
       h2('How To Use'),
-      h3("Manhattan Plot"),
+      h3('Interactive Phewas Manhattan Plot'),
       p("Use the Manhattan plot to select your codes of interest by dragging a box on main plot or searching/selecting with the table."),
       p("Once you have your desired codes selected press 'Update Network' button at top of pane to update the network data with individuals possessing the selected codes."),
-      h3("Upset Plot"),
+      h3('Comorbidity Upset Plot'),
       p("The upset plot allows you to see basic statistics about comorbidity patterns in the selected subset of codes, such as number of patients with a pattern and the risk of that pattern occuring in individuals with at least one copy of the minor allele."),
       p("Clicking on a given pattern in the upset plot will highlight the patients with that pattern in the below network plot."),
-      h3("Network Plot"),
+      h3('Subject-Phecode Bipartite Network'),
       p("The network plot provides a direct look at the individual-level data. You can click on codes to select them for isolation or deletion from the current selection.")
     )
   } else {
