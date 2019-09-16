2348edf442fa966b1b1aca0829d58856e4a1e92a
Nick Strayer <nick.strayer@gmail.com>
2019-09-16 21:59:16 +0000

    Added tests for data validating functions and improved error messages and conditions for the phewas testing function.

--- R/checkphewasfile.R
+++ R/checkphewasfile.R
@@ -10,16 +10,17 @@
 #' checkPhewasFile('uploadedPhewasFile')
 checkPhewasFile <- function(phewas){
 
-  columns <- colnames(phewas)
+  required_columns <- c('code', 'p_val', 'OR', 'description', 'category')
 
-  has_code <- 'code' %in% columns
-  if(!has_code) stop("Missing Code column.", call. = FALSE)
+  missing_columns <- required_columns[!(required_columns %in% colnames(phewas))]
 
-  has_category <- 'category' %in% columns
-  if(!has_category) stop("Missing Category column", call. = FALSE)
-
-  has_pval <- 'p_val' %in% columns
-  if(!has_pval) stop("Missing P Value column (p_val)", call. = FALSE)
+  num_missing <- length(missing_columns)
+  if (num_missing == 1) {
+    stop(paste('Missing', missing_columns, 'column.'), call. = FALSE)
+  } else if (num_missing > 1) {
+    missing_columns[num_missing] <- paste('and',  missing_columns[num_missing])
+    stop(paste0('Missing columns ', paste(missing_columns, collapse = ', '), '.'), call. = FALSE)
+  }
 
   phewas
 }
