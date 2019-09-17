82e27ed5e4f9cf3b6a9bb86dbc25e13c1a1e5657
Nick Strayer <nick.strayer@gmail.com>
2019-09-17 16:57:52 -0500

    Readded library metoolkit loading to demo app for uploading to connect. Needs to be toggled on and off with a comment when testing etc.

--- inst/demo_app/app.R
+++ inst/demo_app/app.R
@@ -1,7 +1,8 @@
 # testing main app module in own shiny app.
 library(shiny)
 library(readr)
-devtools::load_all('../')
+library(meToolkit)
+# devtools::load_all('../')
 
 phewas_results <- readr::read_csv('data/phewas_results.csv')
 id_to_snp <-      readr::read_csv('data/id_to_snp.csv')
