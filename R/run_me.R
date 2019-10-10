8d00577eba7bfa3b0133f7de474bde287d1b5363
Nick Strayer <nick.strayer@gmail.com>
2019-10-09 19:47:55 -0500

    Back button still isn't working but the underlying framework should make it easier to get it working in the future.

--- R/run_me.R
+++ R/run_me.R
@@ -22,25 +22,19 @@ run_me <- function(preloaded_path = NULL, auto_run = FALSE){
 
  app_server <- function(input, output, session) {
 
+   not_loaded_msg <- 'not loaded'
    # Main reactive that stores our data
    app_data <- reactiveVal()
-   app_data('not loaded')
+
+   app_data(not_loaded_msg)
 
    have_no_data <- reactive({
-     any(app_data() == 'not loaded')
+     any(app_data() == not_loaded_msg)
    })
-   # Call data loading module to kick things off
-   # data_loader_results <- shiny::callModule(
-   #  meToolkit::data_loader,
-   #  'data_loader',
-   #  preloaded_path = preloaded_path)
-
 
    shiny::observeEvent(have_no_data(),{
 
      if(have_no_data()) {
-       print('Rendering data loading screen')
-
       data_loader_results <- shiny::callModule(
         meToolkit::data_loader,
         'data_loader',
@@ -50,11 +44,12 @@ run_me <- function(preloaded_path = NULL, auto_run = FALSE){
       shiny::observeEvent(data_loader_results(), {
         # Fill in app data reactive with results from data loader.
         app_data(data_loader_results())
+        # Reset data loader
+        data_loader_results(NULL)
       })
 
      } else{
        print('Rendering main dashboard')
-
        # Show main app dashboard
        shiny::callModule(
          meToolkit::main_dashboard,
@@ -74,7 +69,7 @@ run_me <- function(preloaded_path = NULL, auto_run = FALSE){
      print('User clicked the back button')
 
      # Let app know data is loaded
-     app_data(NULL)
+     app_data(not_loaded_msg)
    })
 
    output$ui <- shiny::renderUI({
@@ -85,21 +80,7 @@ run_me <- function(preloaded_path = NULL, auto_run = FALSE){
        meToolkit::main_dashboard_UI("main_app")
      }
    })
-   # #
-   # shiny::observe({
-   #   shiny::req(app_data())
-   #
-   # })
-   #
 
-
-   # # Watch back button for press
-   # shiny::observeEvent(input$back_button_clicked, {
-   #   print('User clicked the back button')
-   #
-   #   # Let app know data is loaded
-   #   app_data(NULL)
-   # })
  }
 
  if (auto_run) {
