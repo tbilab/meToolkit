e46f313b792f177f7a3a26a41a7e7efbfdf1e06a
Nick Strayer <nick.strayer@gmail.com>
2020-04-27 17:43:55 -0500

    Reformatted code in main_dashboard module

--- R/mod_main_dashboard.R
+++ R/mod_main_dashboard.R
@@ -24,8 +24,7 @@ main_dashboard_UI <- function(id, snp_colors) {
       upset = meToolkit::upset_UI(ns('upset_plot_main_dashboard')),
       network_title = 'Subject-Phecode Bipartite Network',
       network = meToolkit::network_plot_UI(ns('network_plot_main_dashboard'),
-                                 snp_colors = snp_colors
-      ),
+                                           snp_colors = snp_colors),
       info_panel = meToolkit::info_panel_UI(ns('info_panel_main_dashboard'))
     )
   )
@@ -68,47 +67,57 @@ main_dashboard_UI <- function(id, snp_colors) {
 #'   my_individual_data,
 #'   usage_instructions = 'This app is complicated!'
 #' )
-main_dashboard <- function(
-  input, output, session,
-  snp_name,
-  phewas_results,
-  individual_data,
-  max_allowed_codes = 40,
-  usage_instructions = 'default',
-  colors = list(
-    light_grey = "#f7f7f7",
-    med_grey   = "#d9d9d9",
-    light_blue = "#4292c6",
-    green      = "#74c476"
-  ),
-  snp_colors,
-  debug_mode = FALSE,
-  show_back_button_messenger = NULL
- ) {
-
+main_dashboard <- function(input,
+                           output,
+                           session,
+                           snp_name,
+                           phewas_results,
+                           individual_data,
+                           max_allowed_codes = 40,
+                           usage_instructions = 'default',
+                           colors = list(
+                             light_grey = "#f7f7f7",
+                             med_grey   = "#d9d9d9",
+                             light_blue = "#4292c6",
+                             green      = "#74c476"
+                           ),
+                           snp_colors,
+                           debug_mode = FALSE,
+                           show_back_button_messenger = NULL) {
   # Fill in color info for snp data
   colors["dark_grey"] = snp_colors[1]
   colors["light_red"] = snp_colors[2]
   colors["dark_red"] = snp_colors[3]
 
-  if(usage_instructions == 'default'){
+  if (usage_instructions == 'default') {
     app_instructions <- div(
       h2('How To Use'),
       h3('Interactive Phewas Manhattan Plot'),
-      p("Use the Manhattan plot to select your codes of interest by dragging a box on main plot or searching/selecting with the table."),
-      p("Once you have your desired codes selected press 'Update Network' button at top of pane to update the network data with individuals possessing the selected codes."),
+      p(
+        "Use the Manhattan plot to select your codes of interest by dragging a box on main plot or searching/selecting with the table."
+      ),
+      p(
+        "Once you have your desired codes selected press 'Update Network' button at top of pane to update the network data with individuals possessing the selected codes."
+      ),
       h3('Comorbidity Upset Plot'),
-      p("The upset plot allows you to see basic statistics about comorbidity patterns in the selected subset of codes, such as number of patients with a pattern and the risk of that pattern occuring in individuals with at least one copy of the minor allele."),
-      p("Clicking on a given pattern in the upset plot will highlight the patients with that pattern in the below network plot."),
+      p(
+        "The upset plot allows you to see basic statistics about comorbidity patterns in the selected subset of codes, such as number of patients with a pattern and the risk of that pattern occuring in individuals with at least one copy of the minor allele."
+      ),
+      p(
+        "Clicking on a given pattern in the upset plot will highlight the patients with that pattern in the below network plot."
+      ),
       h3('Subject-Phecode Bipartite Network'),
-      p("The network plot provides a direct look at the individual-level data. You can click on codes to select them for isolation or deletion from the current selection.")
+      p(
+        "The network plot provides a direct look at the individual-level data. You can click on codes to select them for isolation or deletion from the current selection."
+      )
     )
   } else {
     app_instructions <- usage_instructions
   }
 
   # Add colors to codes in results data.
-  phewas_results <- meToolkit::buildColorPalette(phewas_results, category)
+  phewas_results <-
+    meToolkit::buildColorPalette(phewas_results, category)
 
   # Get available codes sorted by p-value
   available_codes <- phewas_results %>%
@@ -124,9 +133,9 @@ main_dashboard <- function(
   starting_codes <- c()
 
   # If the user has requested some codes to be loaded via the URL...
-  if(!is.null(requested_codes)){
+  if (!is.null(requested_codes)) {
     # Only attempt to load codes if requested snp is what we are currently looking at
-    if(desired_snp == snp_name){
+    if (desired_snp == snp_name) {
       # Make sure that we actually have these codes...
       starting_codes <- intersect(requested_codes, available_codes)
     }
@@ -134,7 +143,7 @@ main_dashboard <- function(
 
   # Fall back to using the five most significant codes if nothing was suggested
   # or no codes of the suggested could be found
-  if(length(starting_codes) == 0){
+  if (length(starting_codes) == 0) {
     starting_codes <- head(available_codes, 5)
   }
 
@@ -159,7 +168,6 @@ main_dashboard <- function(
   #----------------------------------------------------------------
   # Individual data subset by the currently viewed phecodes and if we've filtered the snp
   curr_ind_data <- shiny::reactive({
-
     keep_everyone <- !state$snp_filter()
     # Filter the individual data to just MA carriers if needed, otw keep everyone
 
@@ -191,32 +199,36 @@ main_dashboard <- function(
   app_interaction <- shiny::reactiveVal()
 
   # Function to retreive codes from an action payload
-  extract_codes <- function(payload){
-    tail(unlist(payload), -1)
+  extract_codes <- function(payload) {
+    tail(unlist(payload),-1)
   }
 
-  shiny::observeEvent(app_interaction(),{
+  shiny::observeEvent(app_interaction(), {
     action_type <- app_interaction()[['type']]
     action_payload <- app_interaction()[['payload']]
 
-    if(debug_mode){
-      print(glue::glue("========================
+    if (debug_mode) {
+      print(
+        glue::glue(
+          "========================
                        Action:{action_type}
-                       Payload:{jsonlite::toJSON(action_payload)}"))
+                       Payload:{jsonlite::toJSON(action_payload)}"
+        )
+      )
     }
 
-    bad_request_msg <- function(num_requested = 1){
-      if(num_requested < 2){
-        meToolkit::pretty_popup(
-          session,
-          "Too few codes requested",
-          "Try selecting at least two codes."
-        )
+    bad_request_msg <- function(num_requested = 1) {
+      if (num_requested < 2) {
+        meToolkit::pretty_popup(session,
+                                "Too few codes requested",
+                                "Try selecting at least two codes.")
       } else {
         meToolkit::pretty_popup(
           session,
           "Too many codes requested",
-          glue::glue("The maximum allowed is {max_allowed_codes} and {num_requested} were selected. \n\n This is so your computer doesn't explode. Try a smaller selection. Sorry!")
+          glue::glue(
+            "The maximum allowed is {max_allowed_codes} and {num_requested} were selected. \n\n This is so your computer doesn't explode. Try a smaller selection. Sorry!"
+          )
         )
       }
     }
@@ -226,16 +238,15 @@ main_dashboard <- function(
         delete = {
           codes_to_delete <- extract_codes(action_payload)
           prev_selected_codes <- state$selected_codes()
-          state$selected_codes(
-            prev_selected_codes[!(prev_selected_codes %in% codes_to_delete)]
-          )
+          state$selected_codes(prev_selected_codes[!(prev_selected_codes %in% codes_to_delete)])
         },
         selection = {
           codes_to_select <- extract_codes(action_payload)
           num_requested_codes <- length(codes_to_select)
 
           # Check size of request.
-          if((num_requested_codes < 2 )| (num_requested_codes > max_allowed_codes)){
+          if ((num_requested_codes < 2) |
+              (num_requested_codes > max_allowed_codes)) {
             bad_request_msg(num_requested_codes)
           } else {
             state$selected_codes(codes_to_select)
@@ -243,7 +254,7 @@ main_dashboard <- function(
         },
         isolate = {
           desired_codes <- extract_codes(action_payload)
-          if(length(desired_codes) < 2){
+          if (length(desired_codes) < 2) {
             bad_request_msg(length(desired_codes))
           } else {
             state$selected_codes(desired_codes)
@@ -254,30 +265,25 @@ main_dashboard <- function(
         },
         pattern_highlight = {
           # Highlight all nodes with specific pattern
-          state$highlighted_pattern(
-            list(type = 'pattern', codes = extract_codes(action_payload))
-          )
+          state$highlighted_pattern(list(type = 'pattern', codes = extract_codes(action_payload)))
         },
         code_highlight = {
           # Highlight all nodes who have a connection to a given code.
-          state$highlighted_pattern(
-            list(type = 'code', codes = extract_codes(action_payload)[1])
-          )
+          state$highlighted_pattern(list(type = 'code', codes = extract_codes(action_payload)[1]))
         },
         invert = {
           currently_inverted <- state$inverted_codes()
           requested_inversion <- extract_codes(action_payload)
-          new_inverted_list <- meToolkit::invertCodes(requested_inversion, currently_inverted)
+          new_inverted_list <-
+            meToolkit::invertCodes(requested_inversion, currently_inverted)
           state$inverted_codes(new_inverted_list)
         },
         stop("Unknown input")
       )
 
     # make sure the code highlights are reset when the code selections change.
-    if (action_type %in% c('delete', 'isolate', 'selection', 'invert')){
-      state$highlighted_pattern(
-        list(type = 'pattern', codes = c())
-      )
+    if (action_type %in% c('delete', 'isolate', 'selection', 'invert')) {
+      state$highlighted_pattern(list(type = 'pattern', codes = c()))
     }
 
     # Update the URL of the app so user's can return to point easily
@@ -289,7 +295,8 @@ main_dashboard <- function(
   #----------------------------------------------------------------
   ## Network plot
   shiny::callModule(
-    meToolkit::network_plot, 'network_plot_main_dashboard',
+    meToolkit::network_plot,
+    'network_plot_main_dashboard',
     network_data = curr_network_data,
     highlighted_codes = state$highlighted_pattern,
     snp_filter = state$snp_filter,
@@ -300,7 +307,8 @@ main_dashboard <- function(
 
   ## Upset plot
   shiny::callModule(
-    meToolkit::upset, 'upset_plot_main_dashboard',
+    meToolkit::upset,
+    'upset_plot_main_dashboard',
     individual_data = curr_ind_data,
     all_patient_snps = dplyr::select(individual_data, IID, snp),
     results_data = phewas_results,
@@ -310,7 +318,8 @@ main_dashboard <- function(
 
   ## Manhattan plot
   shiny::callModule(
-    meToolkit::manhattan_plot_and_table, 'manhattan_plot_main_dashboard',
+    meToolkit::manhattan_plot_and_table,
+    'manhattan_plot_main_dashboard',
     results_data = phewas_results,
     selected_codes = state$selected_codes,
     action_object = app_interaction,
@@ -319,7 +328,8 @@ main_dashboard <- function(
 
   # SNP info panel
   shiny::callModule(
-    info_panel, 'info_panel_main_dashboard',
+    info_panel,
+    'info_panel_main_dashboard',
     snp_name = snp_name,
     all_individual_data = individual_data,
     instructions = app_instructions,
@@ -330,19 +340,22 @@ main_dashboard <- function(
   # Multicode selecter input
   shiny::observeEvent(input$filter_to_desired, {
     codes_desired <- input$desired_codes
-    action_object_message <-  list(
-      type = 'selection',
-      payload = codes_desired
-    )
+    action_object_message <-  list(type = 'selection',
+                                   payload = codes_desired)
     app_interaction(action_object_message)
   })
 
 
   # If we are in the full data-loader + dashboard setup, enable back button
   should_enable_back_button = !is.null(show_back_button_messenger)
-  if(should_enable_back_button){
-    print('Enabling back button');
+  if (should_enable_back_button) {
+    print('Enabling back button')
+
     # Enable back button
-    show_back_button_messenger("show_back_button", "<span class='back-arrow'>&#10554;</span> Return to data loader");
+    show_back_button_messenger(
+      "show_back_button",
+      "<span class='back-arrow'>&#10554;</span> Return to data loader"
+    )
+
   }
 }
