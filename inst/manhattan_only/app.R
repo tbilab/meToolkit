c16072b7782b1f5f858659ff29848ef95247154e
Nick Strayer <nick.strayer@gmail.com>
2020-06-09 21:30:00 -0500

    There was no reason to load the individual data

--- inst/manhattan_only/app.R
+++ inst/manhattan_only/app.R
@@ -1,6 +1,7 @@
 # testing main app module in own shiny app.
 library(shiny)
 library(readr)
+# Run install_github("tbilab/meToolkit") to make sure you have the latest version
 library(meToolkit)
 
 app_title <- "Just Manhattan Plot"
@@ -13,32 +14,13 @@ phewas_results <- readr::read_csv(here::here('inst/demo_app/data/phewas_results.
 #   2 401.30 Other hypertensive complications                 1.47  0.0588 circulatory system
 #   3 411.00 Ischemic Heart Disease                           0.848 0.283  circulatory system
 
-id_to_snp <-      readr::read_csv(here::here('inst/demo_app/data/id_to_snp.csv'))
-# > id_to_snp
-# # A tibble: 1,000 x 2
-#     IID   rs123456
-#     <chr>    <dbl>
-#   1 r1           1
-#   2 r10          0
-#   3 r100         0
-#   4 r1000        0
-
-id_to_phenome <- readr::read_csv(here::here('inst/demo_app/data/id_to_phenome.csv'))
-# > id_to_phenome
-# # A tibble: 43,316 x 2
-#     IID   code
-#     <chr> <chr>
-#   1 r1    008.00
-#   2 r104  008.00
-#   3 r145  008.00
-#   4 r186  008.00
-#   5 r188  008.00
-
-# Setup data in the module-friendly combined format
-data_for_shiny <- reconcile_data(phewas_results, id_to_snp, id_to_phenome, "none")
-data_for_shiny$phewas_results <- data_for_shiny$phewas_results
+# Add category colors to data
 cat_colors <- phewasHelper::category_colors()
-data_for_shiny$phewas_results$color <- cat_colors[data_for_shiny$phewas_results$category]
+phewas_results$color <- cat_colors[phewas_results$category]
+
+selected_codes <- phewas_results %>%
+  dplyr::filter(p_val < 0.01) %>%
+  dplyr::pull(code)
 
 styles <- "
   body {
@@ -78,6 +60,36 @@ styles <- "
   }
 
 "
+colors <-  list(
+  light_grey = "#f7f7f7",
+  med_grey   = "#d9d9d9",
+  light_blue = "#4292c6",
+  green      = "#74c476"
+)
+r2d3::r2d3(
+  data = phewas_results,
+  script = system.file("d3/manhattan_plot/manhattan_plot.js", package = "meToolkit"),
+  container = 'div',
+  dependencies = c(
+    "d3-jetpack",
+    system.file("d3/helpers.js", package = "meToolkit"),
+    system.file("d3/manhattan_plot/phewas_table.js", package = "meToolkit"),
+    system.file("d3/manhattan_plot/clusterize.js", package = "meToolkit")
+  ),
+  css = c(
+    system.file("d3/helpers.css", package = "meToolkit"),
+    system.file("d3/manhattan_plot/manhattan_plot.css", package = "meToolkit"),
+    system.file("css/common.css", package = "meToolkit")
+  ),
+  options = list(
+    msg_loc = "standalone",
+    selected = selected_codes,
+    sig_bar_locs = "0.01",
+    colors = colors,
+    timestamp = 1
+  )
+)
+
 app_ui <- shiny::htmlTemplate(
   system.file("html_templates/empty_page.html", package = "meToolkit"),
   app_content = tagList(
@@ -91,17 +103,14 @@ app_ui <- shiny::htmlTemplate(
 )
 
 app_server <- function(input, output, session) {
-  selected_codes <- data_for_shiny$phewas_results %>%
-    dplyr::filter(p_val < 0.01) %>%
-    dplyr::pull(code) %>%
-    shiny::reactiveVal()
+
 
   ## Manhattan plot
   shiny::callModule(
     meToolkit::manhattan_plot_and_table,
     'manhattan_plot_and_table',
-    results_data = data_for_shiny$phewas_results,
-    selected_codes = selected_codes,
+    results_data = phewas_results,
+    selected_codes = shiny::reactiveVal(selected_codes),
     colors = list(
       light_grey = "#f7f7f7",
       med_grey   = "#d9d9d9",
